9c8440c5caa8616a81e2b0394222a32fde048f4b
Axl Francois <axl.francois@ugent.be>
2018-03-01 19:18:29 +0100

    Solver

---
+++ GoogleHashCode2018/src/solver/Solver.java
@@ -0,0 +1,17 @@
+package solver;
+
+import domain.Ride;
+import domain.Vehicle;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Sklipnoty
+ */
+public class Solver {
+     private Map<Vehicle, List<Ride>> rides = new HashMap<>();
+     
+     
+}
